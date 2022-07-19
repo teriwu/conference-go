abce12f678cac24b1bcdd25970b9d39d669e0a0d
Teresa Wu <teresa.wu.jy@gmail.com>
2022-07-19 14:35:09 -0700

    D2: Dyanmic HTML, Showing The Image

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,35 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
 
+    const url = "http://localhost:8000/api/conferences/";
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            // Figure out what to do when the response is bad
+        } else {
+          const data = await response.json();
+
+          const conference = data.conferences[0];
+          const nameTag = document.querySelector(".card-title");
+          nameTag.innerHTML = conference.name;
+          
+          const detailUrl = `http://localhost:8000${conference.href}`;
+          const detailResponse = await fetch(detailUrl);
+          if (detailResponse.ok) {
+            const details = await detailResponse.json();
+            console.log(details);
+            const detailTag = document.querySelector(".card-text");
+            detailTag.innerHTML = details.conference.description;
+            
+            const imageTag = document.querySelector(".card-img-top");
+            imageTag.src = details.conference.location.picture_url;
+            imageTag.innerHTML;
+          }
+        }
+
+    } catch (e) {
+        // Figure out what to do if an error is raised
+        console.log(`Error: ${e.name}`);
+    }
 
 });
--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -9,7 +9,8 @@ from .models import Conference, Location, State
 
 class LocationListEncoder(ModelEncoder):
     model = Location
-    properties = ["name"]
+    properties = ["name", "picture_url"]
+
 
 
 class LocationDetailEncoder(ModelEncoder):
