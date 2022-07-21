9dd8326b007b9f8801d3bb433d554f3e102845a0
Teresa Wu <teresa.wu.jy@gmail.com>
2022-07-21 16:59:06 -0700

    Sign In, djwto

--- ghi/js/attend-conference.js
+++ ghi/js/attend-conference.js
@@ -12,11 +12,37 @@ window.addEventListener("DOMContentLoaded", async () => {
         option.innerHTML = conference.name;
         selectTag.appendChild(option);
       }
+
       // Add "d-none" class to the loading icon
       const loadingTag = document.getElementById("loading-conference-spinner");
       loadingTag.classList.add("d-none");
+
       // Remove "d-none" class from the conference tag (selectTag)
       selectTag.classList.remove("d-none");
+
+      //
+      const formTag = document.getElementById("create-attendee-form");
+        formTag.addEventListener("submit", async (event) => {
+            event.preventDefault();
+            const formData = new FormData(formTag);
+            const json = JSON.stringify(Object.fromEntries(formData));
+            const attendeeUrl = "http://localhost:8001/api/attendees/";
+            const fetchConfig = {
+                method: "post",
+                body: json,
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+            const response = await fetch(attendeeUrl, fetchConfig);
+            if (response.ok) {
+                const successTag = document.getElementById("success-message");
+                successTag.classList.remove("d-none");
+                formTag.classList.add("d-none");
+                const newAttendee = await response.json();
+                console.log(newAttendee);
+            }
+        })
     }
-  
-  });
+    
+});
