e137eefb473d753aae7de8c3adf3de005a0e8a01
Teresa Wu <teresa.wu.jy@gmail.com>
2022-07-25 11:52:51 -0700

    Stretch goal work in progress

---
+++ ghi/js/new-presentation.js
@@ -0,0 +1,45 @@
+window.addEventListener("DOMContentLoaded", async () => {
+
+    const url = "http://localhost:8000/api/conferences/";
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Response not ok");
+        } else {
+            const data = await response.json();
+            const selectTag = document.getElementById("conference");
+            for (let conference of data.conferences) {
+                let option = document.createElement("option");
+                option.value = conference.id;
+                option.innerText = conference.name;
+                selectTag.appendChild(option);
+            }
+            const formTag = document.getElementById("create-presentation-form");
+            formTag.addEventListener("submit", async event => {
+                event.preventDefault();
+                const formData = new FormData(formTag);
+                const json = JSON.stringify(Object.fromEntries(formData));
+                console.log(json);
+                const conferenceId = document.getElementById("conference").value;
+                const presentationUrl = `http://localhost:8000/api/conferences/${conferenceId}/presentations/`;
+                const fetchConfig = {
+                    method: "POST",
+                    body: json,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+                const response = await fetch(presentationUrl, fetchConfig)
+                if (response.ok) {
+                    formTag.reset();
+                    const newPresentation = await response.json();
+                    console.log(newPresentation);
+                }
+            })
+        }
+    } catch (e) {
+        
+    }
+});
